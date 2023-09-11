5d0179f7e778d168ccb1147ebda52b5e7bd791ea
Dennise Catolos <dentolos19@gmail.com>
2023-09-11 08:57:29 +0800

    Removed extra properties in workouts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,10 +2,4 @@ export type Workout = {
   name: string;
   category: string;
   url?: string;
-  performance: {
-    time: number;
-    weight: number;
-    reps: number;
-    sets: number;
-  };
 };
