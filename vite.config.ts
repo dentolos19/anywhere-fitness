82ee706c8c932da3428f5e7ff0d00685071b645e
Dennise Catolos <contact@dennise.me>
2023-10-11 16:40:09 +0800

    Changed some stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,10 @@
 import react from "@vitejs/plugin-react-swc";
 import * as path from "path";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "Anywhere Fitness",
-        short_name: "AWF",
-      },
-    }),
-  ],
+  plugins: [react()],
   resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: path.resolve(__dirname, "src"),
-      },
-    ],
+    alias: { "@": path.resolve(__dirname, "src") },
   },
 });
